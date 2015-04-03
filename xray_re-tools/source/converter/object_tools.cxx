6188fb6aa9a165b8f824821a0da2a4e93a566597
Viнt@rь <>
2015-04-03 10:51:45 +0300

    Первый коммит с проектом уишки:   Наброски интерфейса. Drag`n`Drop. Конвертирование моделек/анимаций из формата движка в формат сдк.

--- xray_re-tools/source/converter/object_tools.cxx
+++ xray_re-tools/source/converter/object_tools.cxx
@@ -2,6 +2,7 @@
 #include "xr_object.h"
 #include "xr_object_format.h"
 #include "xr_file_system.h"
+#include "xr_string_utils.h"
 
 using namespace xray_re;
 
@@ -17,7 +18,7 @@ void object_tools::process(const cl_parser& cl)
 }
 #endif
 
-void object_tools::save_object(xray_re::xr_object& object, const char* source) const
+void object_tools::save_object(xr_object& object, const char* source) const
 {
 	std::string target;
 	make_target_name(target, source, ".object");
@@ -26,75 +27,7 @@ void object_tools::save_object(xray_re::xr_object& object, const char* source) c
 		msg("can't save object in %s", target.c_str());
 }
 
-void object_tools::save_bones(xray_re::xr_object& object, const char* source) const
-{
-	if (object.bones().empty()) {
-		msg("game object has no bones");
-	} else {
-		std::string target;
-		make_target_name(target, source, ".bones");
-		if (!object.save_bones(target.c_str()))
-			msg("can't save bones in %s", target.c_str());
-	}
-}
-
-void object_tools::save_skls(xray_re::xr_object& object, const char* source) const
-{
-	if (object.motions().empty()) {
-		msg("game object has no own motions");
-	} else {
-		std::string target;
-		make_target_name(target, source, ".skls");
-		if (!object.save_skls(target.c_str()))
-			msg("can't save motions in %s", target.c_str());
-	}
-}
-
-void object_tools::save_skl(xray_re::xr_object& object, const char* source, const cl_parser& cl) const
-{
-	std::string motion_name;
-	cl.get_string("-skl", motion_name);
-	if (motion_name != "all") {
-		xr_skl_motion* smotion = object.find_motion(motion_name);
-		if (smotion) {
-			std::string target;
-			if (m_output_file.empty()) {
-				std::string name;
-				xr_file_system::split_path(source, 0, &name);
-				target = m_output_folder;
-				target += name;
-				target += '_';
-				target += motion_name;
-				target += ".skl";
-			} else {
-				target = m_output_file;
-			}
-			if (!smotion->save_skl(target.c_str()))
-				msg("can't save %s", target.c_str());
-		} else {
-			msg("can't find motion %s", motion_name.c_str());
-		}
-	} else {
-		for (xr_skl_motion_vec_it it = object.motions().begin(), end = object.motions().end(); it != end; ++it) {
-			std::string name, target;
-			xr_skl_motion* smotion = *it;
-			cl.get_string("-out", name);
-			if (!name.empty()) {
-				xr_file_system& fs = xr_file_system::instance();
-				fs.create_folder(name);
-				target = name;
-				target += "//";
-				target += smotion->name();
-			} else {
-				target = smotion->name();
-			}
-			target += ".skl";
-			if (!smotion->save_skl(target.c_str()))
-				msg("can't save %s", target.c_str());
-		}
-	}
-}
-
+#ifdef _CONSOLE
 object_tools::target_format object_tools::get_target_format(const cl_parser& cl) const
 {
 	unsigned format = 0;
@@ -110,3 +43,18 @@ object_tools::target_format object_tools::get_target_format(const cl_parser& cl)
 		format |= TARGET_OBJECT;
 	return (format & (format - 1)) == 0 ? static_cast<target_format>(format) : TARGET_ERROR;
 }
+#else
+object_tools::target_format object_tools::get_target_format(const char* format) const 
+{
+	if (xr_stricmp(format, "info") == 0)
+		return TARGET_INFO;
+	else if (xr_stricmp(format, "skl") == 0)
+		return TARGET_SKL;
+	else if (xr_stricmp(format, "skls") == 0)
+		return TARGET_SKLS;
+	else if (xr_stricmp(format, "bones") == 0)
+		return TARGET_BONES;
+
+	return TARGET_OBJECT;
+}
+#endif // _CONSOLE
